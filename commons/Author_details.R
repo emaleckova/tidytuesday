b63e2d46004e6ea224574fcbf5a9520674287c8f
EvaMaleckova <eva.maleckova@hhu.de>
2025-08-20 18:29:12 +0200

    Removed | separator

--- commons/Author_details.R
+++ commons/Author_details.R
@@ -12,7 +12,7 @@ font_add("fa-brands", "commons/fonts/Font Awesome 7 Brands-Regular-400.otf")
 
 social_caption <- glue::glue(
   "<span style='font-family:\"fa-brands\"'>{github_icon};</span>
-  <span style='color: #000000'>{github_user}  | </span> <span style='color: #0077B5; font-family:\"fa-brands\"'>{linkedin_icon};</span>
+  <span style='color: #000000'>{github_user} </span> <span style='color: #0077B5; font-family:\"fa-brands\"'>{linkedin_icon};</span>
   <span style='color: #000000 '>{linkedin_user}</span>"
 )
 
