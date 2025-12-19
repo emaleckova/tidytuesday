99e3dfa6b3b6dc580b2e9b29b4a49a11a0bb6aa6
Eva Maleckova <41913488+emaleckova@users.noreply.github.com>
2025-12-19 09:39:17 +0100

    Draft for cumulative counts

--- 2025/2025-12-16/20251216.R
+++ 2025/2025-12-16/20251216.R
@@ -1,13 +1,29 @@
-# Using R
-# Option 1: tidytuesdayR R package 
-## install.packages("tidytuesdayR")
+library(tidytuesdayR)
+library(dplyr)
+library(ggplot2)
 
+# Get data with tidytuesdayR
 tuesdata <- tidytuesdayR::tt_load('2025-12-16')
-## OR
-tuesdata <- tidytuesdayR::tt_load(2025, week = 50)
-
 roundabouts_clean <- tuesdata$roundabouts_clean
 
-# Option 2: Read directly from GitHub
+# Make year a factor and leave out all roundabouts with unknown completion date
+roundabouts_completed <- roundabouts_clean %>% 
+  filter(year_completed != 0)
 
-roundabouts_clean <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-12-16/roundabouts_clean.csv')
+
+roundabouts_cum <- roundabouts_completed %>% 
+  group_by(country, year_completed) %>%
+  summarize(n_rbouts = n()) %>% 
+  arrange(year_completed) %>% 
+  mutate(cum_rbouts = cumsum(n_rbouts))
+
+ggplot(roundabouts_cum, aes(x = year_completed, y = cum_rbouts,
+                            group = country)) +
+  geom_col(aes(colour = country)) +
+  facet_wrap(~country, scales = "free_y") +
+  coord_cartesian(expand = F) +
+  scale_y_continuous(breaks = seq(min(roundabouts_cum$year_completed),
+                                  max(roundabouts_cum$year_completed), length.out = 5)) +
+  theme_classic() +
+  theme(strip.background = element_blank(),
+        legend.position = "none")
